607cf1015c9c9027c478a4015c372dca38a3b226
Baswa Leelasarath <100155059+B-L-SARATH@users.noreply.github.com>
2023-08-28 22:11:53 +0530

    Delete src directory

--- src/store/store.js
+++
@@ -1,7 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import Cartslice from "./Cartslice";
-const store = configureStore({
-  reducer: { cart: Cartslice },
-});
-
-export default store;
