0e3885558811d61f00d68a9106be5dc6ab45f4f1
B-L SARATH <leelsarathbaswa@gmail.com>
2023-08-28 17:07:53 +0530

    first commit

---
+++ backend/Routes/Orderdata.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const router = express.Router();
+const Order = require("../models/Order");
+const jwt = require("jsonwebtoken");
+const secretkey = "mynameissarath";
+
+const auth = (req, res, next) => {
+  const token = req.headers.authorization;
+  if (!token) {
+    return res.status(400).send("no token found");
+  }
+  jwt.verify(token, secretkey, (err, user) => {
+    if (err) {
+      return res.status(400).send("invalid user");
+    } else {
+      next();
+    }
+  });
+};
+
+router.post("/orderdata", async (req, res) => {
+  const ordersdata = req.body.orders;
+  ordersdata.splice(0, 0, { ordertime: req.body.ordertime });
+  try {
+    if (await Order.findOne({ email: req.body.email })) {
+      await Order.findOneAndUpdate(
+        { email: req.body.email },
+        { $push: { orders: req.body.orders } }
+      );
+      res.send("checkout success");
+    } else {
+      await Order.create({
+        email: req.body.email,
+        orders: [ordersdata],
+      });
+      res.send("checkout success");
+    }
+  } catch (err) {
+    console.log(err);
+    res.send(err.message);
+  }
+});
+
+router.post("/myorder", auth, async (req, res) => {
+  try {
+    const data = await Order.findOne({ email: req.body.email });
+    res.status(200).json(data);
+  } catch (err) {
+    res.json({ errors: err.message });
+  }
+});
+module.exports = router;
--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Home from "./screens/Home";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Login from "./screens/Login";
+import Signup from "./screens/Signup";
+import Cart from "./screens/Cart";
+import Myorders from "./screens/Myorders";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route
+        path="*"
+        element={
+          <div className="fs-1 text-white text-center">404 NOT FOUND</div>
+        }
+      />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/myorders" element={<Myorders />} />
+    </Routes>
   );
 }
 
